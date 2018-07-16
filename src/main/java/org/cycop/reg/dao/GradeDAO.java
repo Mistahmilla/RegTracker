5ea973c92bfa71ce021a59c996c47b1af4748a8d
Matt <miller@cycop.org>
2018-07-15 21:01:18 -0400

    move from beans to components

--- src/main/java/org/cycop/reg/dao/GradeDAO.java
+++ src/main/java/org/cycop/reg/dao/GradeDAO.java
@@ -2,20 +2,23 @@ package org.cycop.reg.dao;
 
 import org.cycop.reg.dao.mapper.GradeMapper;
 import org.cycop.reg.dataobjects.Grade;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.util.List;
 
+@Component
 public class GradeDAO {
     private JdbcTemplate jdbcTemplate;
     private GradeMapper gradeMapper;
 
-    public GradeDAO(DataSource dataSource) {
+    @Autowired
+    public void init(DataSource dataSource){
         jdbcTemplate = new JdbcTemplate(dataSource);
         gradeMapper = new GradeMapper();
     }
-
     public List<Grade> get(String gradeCode) {
         String sql = "SELECT * FROM T_GRADE WHERE GRADE_C = ?";
         Object[] params = new Object[1];
--- src/main/java/org/cycop/reg/dao/RoleDAO.java
+++ src/main/java/org/cycop/reg/dao/RoleDAO.java
@@ -2,17 +2,21 @@ package org.cycop.reg.dao;
 
 import org.cycop.reg.dao.mapper.RoleMapper;
 import org.cycop.reg.dataobjects.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.util.List;
 
+@Component
 public class RoleDAO {
 
     private JdbcTemplate jdbcTemplate;
     private RoleMapper roleMapper;
 
-    public RoleDAO(DataSource dataSource){
+    @Autowired
+    public void init(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
         this.roleMapper = new RoleMapper();
     }
