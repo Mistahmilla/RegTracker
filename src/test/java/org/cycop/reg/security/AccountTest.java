db8627525a82b7e22c9b58659fb1724b317e5ee4
Matt <miller@cycop.org>
2018-10-20 13:50:23 -0400

    adding additional unit test coverage

--- src/test/java/org/cycop/reg/security/AccountTest.java
+++ src/test/java/org/cycop/reg/security/AccountTest.java
@@ -2,6 +2,8 @@ package org.cycop.reg.security;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
@@ -14,5 +16,41 @@ public class AccountTest {
         a.addAuthority(auth1);
         a.addAuthority(auth2);
         assertEquals(1, a.getAuthorities().size());
+
+        ArrayList<Authority> authList = new ArrayList<Authority>();
+        authList.add(auth1);
+        authList.add(new Authority("TWO", "TWO", "TWO"));
+        a.setAuthorities(authList);
+        assertEquals(2, a.getAuthorities().size());
+    }
+
+    @Test
+    public void TestAccount(){
+        Account a = new Account();
+        a.setAccountID(1);
+        assertEquals(1, a.getAccountID());
+
+        a.setPassword("notapw");
+        assertEquals("notapw", a.getPassword());
+
+        a.setUsername("test@test.com");
+        assertEquals("test@test.com", a.getUsername());
+
+        a.setPasswordSalt("sodium");
+        assertEquals("sodium", a.getPasswordSalt());
+
+        a.setPasswordExpired(false);
+        assertEquals(true, a.isCredentialsNonExpired());
+
+        a.setAccountLocked(false);
+        assertEquals(true, a.isAccountNonLocked());
+        assertEquals(true, a.isAccountNonExpired());
+
+        a.setAccountVerified(true);
+
+        assertEquals(true, a.isEnabled());
+
+        a.setAccountLocked(true);
+        assertEquals(false, a.isEnabled());
     }
 }
