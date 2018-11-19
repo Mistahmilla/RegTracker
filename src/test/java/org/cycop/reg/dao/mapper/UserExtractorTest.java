239d1aa5b79a243d4981d95d2bd1559793bc4061
Matt <miller@cycop.org>
2018-11-18 21:16:08 -0500

    added unit tests on several mapper classes

--- src/test/java/org/cycop/reg/dao/mapper/UserExtractorTest.java
+++ src/test/java/org/cycop/reg/dao/mapper/UserExtractorTest.java
@@ -62,6 +62,48 @@ public class UserExtractorTest {
 
             assertEquals(1,u.getAccountID());
             assertEquals("email",u.getEmailAddress());
+            assertEquals(true, u.getAccountLocked());
+            assertEquals(true, u.getAccountVerified());
+            assertEquals(true, u.getPasswordExpired());
+        }catch(Exception e){
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testUserExtractor2(){
+        List<Person> plist = new ArrayList<Person>();
+        Person p = new Person();
+        p.setFirstName("John");
+        p.setLastName("Doe");
+        p.setPersonID((long)1);
+        plist.add(p);
+        Mockito.when(personDAO.get((long)1)).thenReturn(plist);
+
+        ResultSet rs = Mockito.mock(ResultSet.class);
+        List<User> list;
+        User u;
+        try {
+            Mockito.when(rs.getLong("ACNT_SID")).thenReturn(Long.valueOf(1));
+            Mockito.when(rs.getLong("PER_SID")).thenReturn(Long.valueOf(1));
+            Mockito.when(rs.getString("EML_AD_X")).thenReturn("email");
+            Mockito.when(rs.getString("ACNT_LOCK_I")).thenReturn("N");
+            Mockito.when(rs.getString("ACNT_VERIFIED_I")).thenReturn("N");
+            Mockito.when(rs.getString("PWD_EXP_I")).thenReturn("N");
+            Mockito.when(rs.getString("ROLE_C")).thenReturn("RC");
+            Mockito.when(rs.getString("ROLE_DS")).thenReturn("RDS");
+            Mockito.when(rs.getTimestamp("CRE_T")).thenReturn(Timestamp.valueOf(LocalDateTime.now()));
+            Mockito.when(rs.getTimestamp("UPD_T")).thenReturn(Timestamp.valueOf(LocalDateTime.now()));
+            Mockito.when(rs.next()).thenReturn(true).thenReturn(false);
+            list = userExtractor.extractData(rs);
+
+            u = list.get(0);
+
+            assertEquals(1,u.getAccountID());
+            assertEquals("email",u.getEmailAddress());
+            assertEquals(false, u.getAccountLocked());
+            assertEquals(false, u.getAccountVerified());
+            assertEquals(false, u.getPasswordExpired());
         }catch(Exception e){
             assert(false);
         }
