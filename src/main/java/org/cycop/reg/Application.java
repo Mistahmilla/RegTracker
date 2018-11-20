8d8a0d9901b970182923123e4f4fec08c3167555
Matt <miller@cycop.org>
2018-11-19 21:07:11 -0500

    change to war file deployment and add back autodeploy

--- src/main/java/org/cycop/reg/Application.java
+++ src/main/java/org/cycop/reg/Application.java
@@ -2,13 +2,14 @@ package org.cycop.reg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableCaching
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
