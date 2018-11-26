f713ec9e927b70da3f41675d2a2ecf49b827a2ab
Matt <miller@cycop.org>
2018-11-26 12:47:39 -0500

    adding unit test coverage

--- src/test/java/org/cycop/reg/controller/UserControllerTest.java
+++ src/test/java/org/cycop/reg/controller/UserControllerTest.java
@@ -3,15 +3,16 @@ package org.cycop.reg.controller;
 import org.cycop.reg.dao.PersonDAO;
 import org.cycop.reg.dao.UserDAO;
 import org.cycop.reg.dataobjects.Person;
+import org.cycop.reg.dataobjects.User;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.testng.Assert.fail;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class UserControllerTest {
 
@@ -21,6 +22,7 @@ public class UserControllerTest {
     @Mock
     UserDAO userDAO;
 
+    @Spy
     @InjectMocks
     UserController userController;
 
@@ -57,5 +59,31 @@ public class UserControllerTest {
         Mockito.verify(personDAO, Mockito.times(0)).saveOrUpdate(p);
         Mockito.verify(userDAO).addPersonToAccount(Long.valueOf(1),Long.valueOf(1));
     }
-    
+
+    @Test
+    public void testUpdateUserBadUser(){
+        List<User> l = new ArrayList();
+        Mockito.when(userController.getUser(Long.valueOf(1))).thenReturn(l);
+        User u = new User();
+        u.setAccountID(Long.valueOf(1));
+        try {
+            userController.updateUser(u);
+            fail("Expected an exception");
+        }catch(NullPointerException e) {
+            assertEquals("User does not exist.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testUpdateUserGoodUser() {
+        List<User> l = new ArrayList();
+        User u = new User();
+        u.setAccountID(Long.valueOf(1));
+        u.setEmailAddress("john@doe.com");
+        u.setPassword("test");
+        l.add(u);
+        Mockito.when(userController.getUser(Long.valueOf(1))).thenReturn(l);
+        userController.updateUser(u);
+        Mockito.verify(userDAO).updateExisting(u, u);
+    }
 }
