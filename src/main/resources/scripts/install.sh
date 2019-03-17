11f8b4cf7c3c25d2c71d778258548a6815068b8e
Matt <miller@cycop.org>
2019-03-17 19:29:27 -0400

    codedeploy

--- src/main/resources/scripts/install.sh
+++ src/main/resources/scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user
-sudo mv RegTracker.war /usr/share/tomcat8/webapps/RegTracker.war
+sudo mv target/RegTracker.war /usr/share/tomcat8/webapps/RegTracker.war
 service tomcat8 restart
